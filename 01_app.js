5d621ffde9d02ed3d2bb12a7cc2e7525366557c6
Simon Roussin <simon.hokaer@hotmail.ca>
2018-03-07 09:24:19 -0500

    arrangement de bug

--- 01_app.js
+++ 01_app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const util = require("util");
 const app = express();
+
+
+
+
 const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient; // le pilote MongoDB
 const ObjectID = require('mongodb').ObjectID;
